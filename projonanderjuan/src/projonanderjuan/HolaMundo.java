fb113b8b7a3ab50ca1898d7fa9196af411dbbb2d
jb113 <jb113@REV>
2024-03-29 13:41:01 +0100

    cambio2 usuario1 local

---
+++ projonanderjuan/src/projonanderjuan/HolaMundo.java
@@ -0,0 +1,16 @@
+package projonanderjuan;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("cambio1 usuario1 local");
+		
+		System.out.println("cambio2 usuario1 local");
+
+
+	}
+
+}
