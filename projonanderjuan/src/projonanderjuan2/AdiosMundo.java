772b6873510b5dce0c934fd8843255f92cba5042
jb113 <jb113@REV>
2024-03-29 16:58:15 +0100

    modificacion en la rama por el primer usuario

---
+++ projonanderjuan/src/projonanderjuan2/AdiosMundo.java
@@ -0,0 +1,12 @@
+package projonanderjuan2;
+
+public class AdiosMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("modificacion en la rama por el primer usuario");
+
+	}
+
+}
